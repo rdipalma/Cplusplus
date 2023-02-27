5b6d7d9b7ec113b908296cee5744672437a0d8fe
rdipalma <113773854+rdipalma@users.noreply.github.com>
2023-02-27 10:44:24 +0100

    Update main.cpp

--- Potenza/main.cpp
+++ Potenza/main.cpp
@@ -5,7 +5,8 @@ using namespace std;
 
 int main(int argc, char *argv[])
 {
-    int base;int esp;
+    int base;
+    int esp;
     do {
         cout<<"Inserisci la base:"<<endl;
         cin>>base;
@@ -17,8 +18,7 @@ int main(int argc, char *argv[])
     int p,i;
     for (p=1,i=0;i<esp;p=p*base,i++) ;
     cout<<"La potenza vale:"<<p<<endl;
-    
-    
+      
     system("PAUSE");
     return EXIT_SUCCESS;
 }
